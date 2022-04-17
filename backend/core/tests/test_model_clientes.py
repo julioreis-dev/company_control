8b7d73a325af9ee3240c7e46193b5ccaa606be69
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-04-16 22:20:55 -0300

    add new test in test_model.py

--- backend/core/tests/test_model_clientes.py
+++ backend/core/tests/test_model_clientes.py
@@ -47,75 +47,84 @@ def test_create_client_should_return_succes(client) -> None:
     assert response.status_code == 201
     response_content = response.json()
     assert response_content["name"] == data_name["name"]
-    assert response_content["address"] == data_name['address']
-    assert response_content["email"] == data_name['email']
-    assert response_content["tel1"] == data_name['tel1']
-    assert response_content["tel2"] == data_name['tel2']
+    assert response_content["address"] == data_name["address"]
+    assert response_content["email"] == data_name["email"]
+    assert response_content["tel1"] == data_name["tel1"]
+    assert response_content["tel2"] == data_name["tel2"]
 
 
 def test_one_client_exist_with_wrong_email_pattern_should_return_fail(client) -> None:
-    Dossie.objects.create(
-        name="Firmino",
-        address="Rua dos inválidos",
-        email="firma2gmail.com",
-        tel1="21985632147",
-        tel2="21985632148",
-    )
-    response = client.post(dossie_url)
+    data_name = {
+        "name": "Firmino",
+        "address": "Rua dos inválidos",
+        "email": "firma2gmail.com",
+        "tel1": "21985632147",
+        "tel2": "21985632148",
+    }
+    response = client.post(path=dossie_url, data=data_name)
     assert response.status_code == 400
     assert json.loads(response.content) == {
-        "email": ["Este campo é obrigatório."],
-        "name": ["Este campo é obrigatório."],
-        "tel1": ["Este campo é obrigatório."],
+        "email": ["Insira um endereço de email válido."],
     }
 
 
 def test_register_only_one_argument_name_should_return_fail(client) -> None:
-    Dossie.objects.create(name="Firmino")
-    response = client.post(path=dossie_url)
+    data_name = {
+        "name": "Firmino",
+    }
+    response = client.post(path=dossie_url, data=data_name)
     assert response.status_code == 400
     assert json.loads(response.content) == {
         "email": ["Este campo é obrigatório."],
-        "name": ["Este campo é obrigatório."],
         "tel1": ["Este campo é obrigatório."],
     }
 
 
 def test_register_only_one_argument_email_should_return_fail(client) -> None:
-    Dossie.objects.create(email="firma@gmail.com")
-    response = client.post(path=dossie_url)
+    data_name = {
+        "email": "firma@gmail.com",
+    }
+    response = client.post(path=dossie_url, data=data_name)
     assert response.status_code == 400
     assert json.loads(response.content) == {
-        "email": ["Este campo é obrigatório."],
         "name": ["Este campo é obrigatório."],
         "tel1": ["Este campo é obrigatório."],
     }
 
 
 def test_register_only_one_argument_tel1_should_return_fail(client) -> None:
-    Dossie.objects.create(tel1="21985632148")
-    response = client.post(path=dossie_url)
+    data_name = {
+        "tel1": "21985632147",
+    }
+    response = client.post(path=dossie_url, data=data_name)
     assert response.status_code == 400
     assert json.loads(response.content) == {
         "email": ["Este campo é obrigatório."],
         "name": ["Este campo é obrigatório."],
-        "tel1": ["Este campo é obrigatório."],
     }
 
 
 def test_one_client_exist_and_has_parms_should_return_succeed(client) -> None:
-    test_client = Dossie.objects.create(
-        name="Firmino",
-        address="Rua dos inválidos",
-        email="firma@gmail.com",
-        tel1="21985632147",
-        tel2="21985632148",
+    Dossie.objects.bulk_create(
+        [
+            Dossie(
+                name="Firmino",
+                address="Rua dos inválidos",
+                email="firma@gmail.com",
+                tel1="21985632147",
+                tel2="21985632148",
+            ),
+            Dossie(
+                name="Firmino2",
+                address="Rua dos inválidos",
+                email="firma2@gmail.com",
+                tel1="21985632127",
+                tel2="21985632128",
+            ),
+        ]
     )
+
     response = client.get(dossie_url)
-    response_content = json.loads(response.content)[0]
+    response_content = json.loads(response.content)
     assert response.status_code == 200
-    assert response_content["name"] == test_client.name
-    assert response_content["address"] == test_client.address
-    assert response_content["email"] == test_client.email
-    assert response_content["tel1"] == test_client.tel1
-    assert response_content["tel2"] == test_client.tel2
+    assert len(response_content) == 2
