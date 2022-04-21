874f136f2ab163f3ad05330d0a313edcfc6304ed
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-04-21 19:45:10 -0300

    add a new class serializer in serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models.models_clientes import Dossie
 from .models.models_params import ParamsUser
+from .models.models_log_actions import LogActions
 
 
 class DossieSerializer(serializers.ModelSerializer):
@@ -12,4 +13,10 @@ class DossieSerializer(serializers.ModelSerializer):
 class ParamsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParamsUser
+        fields = "__all__"
+
+
+class LogsActionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LogActions
         fields = "__all__"
