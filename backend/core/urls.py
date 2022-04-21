9e3575e8c0146a0aee3d2c44cd7ca419124a1a63
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-04-21 19:46:55 -0300

    add a new route in urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import DossieViewSet, ParamsViewsSet
+from .views import DossieViewSet, ParamsViewsSet, LogsActionsViewsSet
 
 
 router = routers.SimpleRouter()
 router.register("clientes", DossieViewSet, basename="dossieview")
 router.register("params", ParamsViewsSet, basename="paramsview")
+router.register("logaction", LogsActionsViewsSet, basename="logsactionview")
 
 urlpatterns = router.urls
