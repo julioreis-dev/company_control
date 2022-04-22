da7e8fbbe44b5f7f4fc70b4896b29753f0db8b7e
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-04-21 22:27:13 -0300

    add a new test in test_model_log_actions.py

--- backend/core/tests/test_model_log_actions.py
+++ backend/core/tests/test_model_log_actions.py
@@ -31,3 +31,25 @@ def test_one_logaction_should_return_succeed(client) -> None:
     assert response_content["name_project"] == test_logaction.name_project
     assert response_content["action_description"] == test_logaction.action_description
     assert response_content["paramsuser"] == test_logaction.id
+
+def test_register_miss_name_project_in_logactions_fail(client) -> None:
+    data_logactions = {
+        "name_project" : "imobiliaria"
+    }
+    response = client.post(path=logs_url, data=data_logactions)
+    assert response.status_code == 400
+    assert json.loads(response.content) == {
+        'action_description': ['Este campo é obrigatório.'],
+        'paramsuser': ['Este campo é obrigatório.']
+    }
+
+def test_register_miss_action_description_in_logactions_fail(client) -> None:
+    data_logactions = {
+        "action_description" : "imobiliaria"
+    }
+    response = client.post(path=logs_url, data=data_logactions)
+    assert response.status_code == 400
+    assert json.loads(response.content) == {
+        'name_project': ['Este campo é obrigatório.'],
+        'paramsuser': ['Este campo é obrigatório.']
+    }
